32771e8d1aab60f60cddc0326ff4517c3834ca56
Sonu-Khare <159264764+Sonu-Khare@users.noreply.github.com>
2024-02-08 19:30:21 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,26 @@
+a=10#The veriables we can assign as A,a,_
+A_B="Sonu"#The name sonu is string which means the collection of letters
+print(a)
+print(A_B)
+w=True
+d=10.99
+print(d)
+print(type(d))#type data type used to find which data type is it
+print(w)
+list1=[3,52,4,[99],"Shaystri"]#list is collection of letters,number means float,string that we can change anytime
+print(list1)
+tuple2=["Parrot","Sparrow" ]#tuple is the fixed values that we cannot change it
+print(tuple2)
+dict2={"name":"Sonu","age":21,"canvote":"True"}#dictronire is used to store values in key values "key:values"
+print(dict2)                                                         #  which num is not written in double cotes
+#Calculator
+a=6
+b=3
+print("The sum of",a,"and",b," is",a+b)
+print("The substraction of",a,"and",b, "is",a-b)
+print("The multiplication of the number is",a*b)
+print("The Exponential of the number is",a**b)#****this keyword is used to the power of the num Ex 5**3=125
+print("The division of the number is",a/b)
+print("The float division of the number is",a//b)#// operator is used to give fixed value (remove the point value) 
+print("The modula of the number is",a%b)
+
